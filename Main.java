d8fe85dba6fa8cd3a3bb7eea2f8849e461bf0a74
yunussmsk <cynsmsk@gmail.com>
2013-12-11 16:06:04 +0200

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,10 @@
+import javax.swing.*;
+
+public class Main {
+	public static void main(String args[]) {
+		Frame frame = new Frame();
+		frame.setLocationRelativeTo(null);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.show();
+	}
+}
